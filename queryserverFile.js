30a1080ef5e75eca42711e9f2482ff42b49bd9e0
MDIftakherHossain <67021268+MDIftakherHossain@users.noreply.github.com>
2020-11-10 19:29:18 +0600

    Update queryserverFile.js

--- queryserverFile.js
+++ queryserverFile.js
@@ -1,7 +1,8 @@
-var http = require('http');
-var url = require('url');
-var fs = require('fs');
+var http = require('http')
+var url = require('url')
+var fs = require('fs')
 
+// create server
 http.createServer(function (req, res) {
   var q = url.parse(req.url, true);
   var filename = "." + q.pathname;
@@ -9,7 +10,8 @@ http.createServer(function (req, res) {
     if (err) {
       res.writeHead(404, {'Content-Type': 'text/html'});
       return res.end("404 Not Found");
-    } 
+    }
+    
     res.writeHead(200, {'Content-Type': 'text/html'});
     res.write(data);
     return res.end();
